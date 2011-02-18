eecf3ddcaccfc27819762e6a025b998853852b18
Ekaterina Krivich [home edidtion] <krivich.ekaterina@gmail.com>
2011-02-18 14:23:14 +0300

    deployment params added

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "cynical-humor"
+set :repository,  "git@github.com:kiote/cynical-humor.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/srv/www/cynical-humor"
+
+server "62.75.191.219", :web
+set :user, "apps"
+set :use_sudo, false
+
+set :branch, "master"
+
+role :web, "62.75.191.219"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+ namespace :deploy do
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
