99d43bec43120ffdd58ce4a92a91b06a96b355c8
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-02-22 17:56:56 +0300

    main page vk button fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     title += ' | '
     title
   end
+
+  def vk_main
+    code = '<div id="vk_like"></div>
+        <script type="text/javascript">
+        VK.Widgets.Like("vk_like", {type: "full"});
+        </script>'
+
+    return code if current_page? root_url
+    ''
+  end
 end
