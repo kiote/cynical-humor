5df658eec88f094d120deb99427dda53b177c3b4
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-02-24 13:02:19 +0300

    per page in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :log_in?, :except => [:show, :index]
 
   def index
-    @posts = Post.all.paginate :page => params[:page]
+    @posts = Post.all.paginate :page => params[:page], :per_page => 10
 
     respond_to do |format|
       format.html
